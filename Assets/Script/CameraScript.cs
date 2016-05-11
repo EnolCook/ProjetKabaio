16f24d4a2f4e837d4417bfe98ed9a5d775383920
EnolCook <n.grange@rubika-edu.com>
2016-05-11 19:21:11 +0200

    FixedLAG

--- Assets/Script/CameraScript.cs
+++ Assets/Script/CameraScript.cs
@@ -21,7 +21,7 @@ public class CameraScript : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void LateUpdate ()
 	{
 		Vector3 PositionToGo = new Vector3 (((Player2.transform.position.x - Player1.transform.position.x) / 2f) + Player1.transform.position.x, ((Player2.transform.position.y - Player1.transform.position.y) / 2f) + Player1.transform.position.y, this.transform.position.z);
 
